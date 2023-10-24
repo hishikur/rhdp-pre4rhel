1ebab0993d52d4267d059c5ff07d60af8d9df05f
hishikur <35244573+hishikur@users.noreply.github.com>
2023-10-24 11:18:11 +0900

    Changes to be committed: 	modified:   00_os_prep.yml 	renamed:    pkg_fix.sh -> re-register.sh 	check-insights.sh

--- re-register.sh
+++ re-register.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sudo cp /etc/rhsm/rhsm.conf /etc/rhsm/rhsm.conf.sat-backup
-sudo cp /etc/rhsm/rhsm.conf.kat-backup /etc/rhsm/rhsm.conf
+#sudo cp /etc/rhsm/rhsm.conf /etc/rhsm/rhsm.conf.sat-backup
+#sudo cp /etc/rhsm/rhsm.conf.kat-backup /etc/rhsm/rhsm.conf
 sudo subscription-manager remove --all
 sudo subscription-manager unregister
 sudo subscription-manager clean
